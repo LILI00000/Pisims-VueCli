84479f05b104579ede74795931f5cb8519a7a6a2
LILI00000 <2130292@163.com>
2019-10-11 23:21:20 +0800

    主页改成主框架并设置了路由

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,57 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container style="height: 100vh; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1']" router>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-goods"></i>人员管理
+            </template>
+            <el-menu-item-group>              
+              <el-menu-item index="/">选项2</el-menu-item>
+              <el-menu-item index="/about">选项2</el-menu-item>
+            </el-menu-item-group>                     
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>单位管理
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+            </el-menu-item-group>            
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-header style="text-align: right; font-size: 36px">
+          事业单位工作人员信息管理系统
+        </el-header>
+
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <style>
+.el-header {
+  background-color: #33CCCC;
+  color: #FFFFFF;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+body {
+  margin: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -29,3 +70,8 @@
   color: #42b983;
 }
 </style>
+<script>
+export default {
+  name: "app"
+};
+</script>
